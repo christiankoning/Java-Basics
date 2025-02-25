09dc0ba20c3fe32e2ebda21b52ea1f5f9e99a6a3
christiankoning <christian@indusource.nl>
2025-02-25 13:13:44 +0100

    Created variables and datatypes

---
+++ day-01-basics/src/VariablesAndDataTypes.java
@@ -0,0 +1,25 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        // Primitive Data Types
+        int myInt = 10;
+        double myDouble = 5.99;
+        char myChar = 'A';
+        boolean myBool = true;
+        float myFloat = 1.5f;
+
+        System.out.println("Integer: " + myInt);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Character: " + myChar);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("Float: " + myFloat);
+
+        // Non Primitive Data Types
+        String myString = "This is non primitive";
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println("String: " + myString);
+        System.out.println("First Number: " + numbers[0]);
+        System.out.println("Second Number: " + numbers[1]);
+        System.out.println("Last Number: " + numbers[4]);
+    }
+}
